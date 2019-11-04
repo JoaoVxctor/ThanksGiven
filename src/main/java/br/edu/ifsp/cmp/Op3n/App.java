d147d595bcd0d52470cb1b0323b1f0a452b0aa04
alissonfelipe1234 <alissonfelipe1234@outlook.com>
2019-11-04 00:28:15 -0300

    testando uma tela com o o spring startado

--- src/main/java/br/edu/ifsp/cmp/Op3n/App.java
+++ src/main/java/br/edu/ifsp/cmp/Op3n/App.java
@@ -6,22 +6,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import javafx.application.Application;
-import javafx.scene.Parent;
 import javafx.stage.Stage;
 
 
 @SpringBootApplication
 public class App extends Application   {
 	private ConfigurableApplicationContext springContext;
-	private Parent rootNode;
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
-
+	
+	@Override
+	public void stop() throws Exception {
+		springContext.close();
+	}
 
 	@Override
-	public void start(Stage stage) throws Exception {
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
 		
 	}
 }
---
+++ src/main/java/br/edu/ifsp/cmp/Op3n/Main.java
@@ -0,0 +1,10 @@
+package br.edu.ifsp.cmp.Op3n;
+
+public class Main {
+
+	public static void main(String[] args) {
+		App.main(args);
+		Tela.main(args);
+	}
+
+}
