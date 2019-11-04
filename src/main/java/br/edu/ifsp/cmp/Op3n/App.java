4d173c0a425cd8c780b15a288d70d163d2cd88e1
alissonfelipe1234 <alissonfelipe1234@outlook.com>
2019-11-03 22:01:27 -0300

    primeira versão do spring application

--- src/main/java/br/edu/ifsp/cmp/Op3n/App.java
+++ src/main/java/br/edu/ifsp/cmp/Op3n/App.java
@@ -1,13 +1,20 @@
 package br.edu.ifsp.cmp.Op3n;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.hibernate.annotations.Parent;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import sun.net.www.ApplicationLaunchException;
+
+@SpringBootApplication
+public class App   {
+	private ConfigurableApplicationContext springContext;
+	private Parent rootNode;
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+
+
 }
