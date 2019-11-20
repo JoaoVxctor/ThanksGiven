57a525549e750d0c414818ad99799b31f70fdc7a
GLenC99 <guilherme.cossu@aluno.ifsp.edu.br>
2019-11-20 18:32:36 -0300

    Atualizacao dos Controller das Telas
    Signed-off-by: GLenC99 <guilherme.cossu@aluno.ifsp.edu.br>

---
+++ src/main/java/br/com/ifsp/ThanksGiven/controller/AdicionarDoacoesController.java
@@ -0,0 +1,44 @@
+package br.com.ifsp.ThanksGiven.controller;
+
+import org.springframework.stereotype.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
+@Controller
+public class AdicionarDoacoesController {
+
+	@FXML
+	private Pane paneLogin;
+
+	@FXML
+	private Button buttonEntrar;
+
+	@FXML
+	private Font x2;
+
+	@FXML
+	private Font x21;
+
+	@FXML
+	private Font x211;
+
+	@FXML
+	private Font x212;
+
+	@FXML
+	private Font x213;
+
+	@FXML
+	void GoBackAction(MouseEvent event) {
+
+	}
+
+	@FXML
+	void clickEntrarLogin(MouseEvent event) {
+
+	}
+
+}
